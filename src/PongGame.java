fa6a56072307730777bc3ebc51ed280e0a6fb0a4
Wesley Hoy <whoy@ey3.tech>
2022-11-20 15:00:57 -0500

    Add comments to PongGame.java.

--- src/PongGame.java
+++ src/PongGame.java
@@ -2,6 +2,8 @@ import java.awt.*;
 
 public class PongGame {
     public static void main(String[] args) {
+
+        // Initial values
         double ballX = 0.5;
         double ballY = 0.5;
         double ballVX = 0.02;
@@ -14,8 +16,10 @@ public class PongGame {
 
 
         while (true){
+            // If hit edge, bounce
             if (ballX > 1 || ballX < 0) ballVX *= -1;
             if (ballY > 1 || ballY < 0) ballVY *= -1;
+            // If hit left edge, die
             if (ballX < 0) {
                 StdDraw.clear();
                 StdDraw.setPenColor(Color.RED);
@@ -27,11 +31,15 @@ public class PongGame {
                 StdDraw.show();
                 break;
             }
+            // If hit paddle, bounce
             if (ballX > 0.1 && ballX < 0.15 && Math.abs(ballY - StdDraw.mouseY()) < 0.1) {
                 ballVX = 0.02;
             }
+            // Move
             ballX += ballVX;
             ballY += ballVY;
+
+            // Render objects
             StdDraw.clear();
             StdDraw.setPenColor(Color.RED);
             StdDraw.filledCircle(ballX, ballY, 0.03);
